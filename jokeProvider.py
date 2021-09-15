77097af163fca7651eb3e9ee5bf189ff2fc77855
Philipp Heise <philipp.heise.2012@gmail.com>
2021-09-15 18:08:22 +0200

    rename the files

--- jokeProvider.py
+++ jokeProvider.py
@@ -1,5 +1,5 @@
 from flask import Flask, request
-import api_data
+import joke_data
 import random
 
 server_port = 5000
@@ -9,9 +9,9 @@ app = Flask(__name__)
 
 def joke():
     language = request.args.get('language', 'de')
-    if(language not in api_data.joke):
+    if(language not in joke_data.joke):
         language = 'de'
-    return random.choice(api_data.joke[language])
+    return random.choice(joke_data.joke[language])
 
 if __name__ == "__main__":
     app.run('0.0.0.0',port=server_port)
